f516b64b94fa67d182956d577def5707007d6a02
Gimpy <jwhite530@gmail.com>
2011-10-25 17:23:07 -0400

    Some small fixes and added a summary printer.  Still a bug with the output though.

--- scyld_gather_node_info.sh
+++ scyld_gather_node_info.sh
@@ -3,7 +3,7 @@
 #Description: Bash script to load and start glusterfs on compute nodes.
 #Written by: Jeff White (jaw171@pitt.edu) of the University of Pittsburgh
 #Based on unlicensed work by: An unknown engineer at Penguin Computing
-#Version Number: 1.0
+#Version Number: 1.1
 #Revision Date: 10-25-2011
 #License: This script is released under version three (3) of the GNU General Public License (GPL) of the FSF, the text of which is available at http://www.fsf.org/licensing/licenses/gpl-3.0.html
 ##This is a free script, you are free to change and redistribute it.  There is NO WARRANTY, to the extent permitted by law.
@@ -42,35 +42,35 @@ while [ "$node" -lt "$num_nodes" ];do
   #Node number
   printf "$node,">>"$output_file"
   #MAC of eth0
-  bpsh n1 /sbin/ifconfig eth0 | awk '/HWaddr/ {print $5;exit}' | tr '\n' ','>>"$output_file"
+  bpsh n$node /sbin/ifconfig eth0 | awk '/HWaddr/ {print $5;exit}' | tr '\n' ','>>"$output_file"
   #MAC of eth1
-  bpsh n1 /sbin/ifconfig eth1 | awk '/HWaddr/ {print $5;exit}' | tr '\n' ','>>"$output_file"
+  bpsh n$node /sbin/ifconfig eth1 | awk '/HWaddr/ {print $5;exit}' | tr '\n' ','>>"$output_file"
   #Type of CPU
-  bpsh n1 awk -F': ' '/model name/ {print $2;exit}' /proc/cpuinfo | tr '\n' ','>>"$output_file"
+  bpsh n$node awk -F': ' '/model name/ {print $2;exit}' /proc/cpuinfo | tr '\n' ','>>"$output_file"
   #Number of CPU cores
-  bpsh n1 egrep -c '^processor' /proc/cpuinfo | tr '\n' ','>>"$output_file"
+  bpsh n$node egrep -c '^processor' /proc/cpuinfo | tr '\n' ','>>"$output_file"
   #GPU found?
-  bpsh n1 lsmod | egrep '^nvidia' >/dev/null
+  bpsh n$node lsmod | egrep '^nvidia' >/dev/null
   if [ "$?" = "0" ];then
     printf "1,">>"$output_file"
   else
     printf "0,">>"$output_file"
   fi
   #Infiniband found?
-  bpsh n1 ifconfig -a | egrep '^ib' >/dev/null
+  bpsh n$node ifconfig -a | egrep '^ib' >/dev/null
   if [ "$?" = "0" ];then
     printf "1,">>"$output_file"
   else
     printf "0,">>"$output_file"
   fi
   #Amount of scratch disk
-  if bpsh n1 df -kP | grep '/scratch'>/dev/null;then #If there is a scratch disk found...
-    bpsh n1 df -kP | awk '/\/scratch/ {print $4/1024/1024;exit}' | tr '\n' ','>>"$output_file"
+  if bpsh n$node df -kP | grep '/scratch'>/dev/null;then #If there is a scratch disk found...
+    bpsh n$node df -kP | awk '/\/scratch/ {print $4/1024/1024;exit}' | tr '\n' ','>>"$output_file"
   else
     printf ",">>"$output_file"
   fi
   #Amount of RAM
-  bpsh n1 awk '/^MemTotal/ {print $2;exit}' /proc/meminfo | tr '\n' ','>>"$output_file"
+  bpsh n$node awk '/^MemTotal/ {print $2;exit}' /proc/meminfo | tr '\n' ','>>"$output_file"
   #Hardware manufacturer
   printf ",">>"$output_file"
   #Hardware model number
@@ -78,7 +78,7 @@ while [ "$node" -lt "$num_nodes" ];do
   #Serial number
   printf ",">>"$output_file"
   #Ethernet IP
-  eth_ip=$(bpsh n1 ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -d':' -f2)
+  eth_ip=$(bpsh n$node ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -d':' -f2)
   echo $eth_ip | tr '\n' ','>>"$output_file"
   #IB IP
   echo $eth_ip | awk -F'.' '{print $1"."$2+2"."$3"."$4}' | tr '\n' ','>>"$output_file"
@@ -88,3 +88,9 @@ while [ "$node" -lt "$num_nodes" ];do
   echo ",">>"$output_file"
   node=$(($node+1))
 done
+
+#Print out a summary of of the cluster
+echo "Nodes: $num_nodes"
+echo "CPU Cores: $(awk -F ',' '{ SUM += $5 } END { printf "%.f\n", SUM }' $output_file)"
+echo "RAM: $(awk -F ',' '{ SUM += $9 } END { printf "%.f\n", SUM }' $output_file)GB"
+echo "Scratch Disk Space: $(awk -F ',' '{ SUM += $8 } END { printf "%.f\n", SUM }' $output_file)GB"

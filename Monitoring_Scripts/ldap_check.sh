f138e650a0e10bb398afbc8f517e388c31bab1b0
Gimpy <jwhite530@gmail.com>
2011-11-03 10:24:28 -0400

    Switched the alert from 'ticket' to 'call'

--- Monitoring_Scripts/ldap_check.sh
+++ Monitoring_Scripts/ldap_check.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #Description: Bash script to check if LDAP is giving valid results.
 #Written By: Jeff White of The Univeristy of Pittsburgh (jaw171@pitt.edu)
-#Version Number: 0.2
-#Revision Date: 11-2-11
 #License: This script is released under version three (3) of the GNU General Public License (GPL) of the FSF, 
 #+the text of which is available at http://www.fsf.org/licensing/licenses/gpl-3.0.html
 ##This is a free script, you are free to change and redistribute it.  There is NO WARRANTY, to the extent permitted by law.
 
 #####
 #
-# Version 0.2 - Added a timeout to the getent command.
+# Version 0.3 - 2011-11-03 - Switched the alert from 'ticket' to 'call'. - Jeff White
+#
+# Version 0.2 - 2011-11-02 - Added a timeout to the getent command. - Jeff White
 #
 #####
 

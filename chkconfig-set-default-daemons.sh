9e2d2837a0c94ab3ab5280b51aacf40ef55d051a
Gimpy <jwhite530@gmail.com>
2011-11-09 15:27:44 -0500

    Complete re-write.

--- chkconfig-set-default-daemons.sh
+++ chkconfig-set-default-daemons.sh
@@ -1,35 +1,68 @@
 #!/bin/bash
 #Description: Bash script to control startup daemons with chkconfig.
 #Written By: Jeff White of The Univeristy of Pittsburgh (jaw171@pitt.edu)
-#Version Number: 0.2
-#Revision Date: 6-22-11
-#License: This script is released under version three (3) of the GNU General Public License (GPL) of the FSF, 
-#+the text of which is available at http://www.fsf.org/licensing/licenses/gpl-3.0.html
-##This is a free script, you are free to change and redistribute it.  There is NO WARRANTY, to the extent permitted by law.
 
+##### License:
+# This script is released under version three (3) of the GNU General Public License (GPL) of the 
+# Free Software Foundation (FSF), the text of which is available at http://www.fsf.org/licensing/licenses/gpl-3.0.html.
+# Use of this script implies your acceptance of this license and its terms.
+# This is a free script, you are free to change and redistribute it with the terms of the GNU GPL.
+# There is NO WARRANTY, not even for FITNESS FOR A PARTICULAR USE to the extent permitted by U.S. law.
+#####
+
+##### Notes:
+# This was written for an environment where GlusterFS is used for home directories and the directory structure 
+# looks like /home/cssd/jaw171 where /home is where the volume is mounted, cssd is the group name, and jaw171 is the user
+# name.  This script is designed to figure out that it should check the directory quota of /cssd if any of '-u jaw171', 
+# '-g cssd', or '-d /cssd' is given as an arguement.  See the _print_usage function for more info.
+#####
+
+##### Revision history:
+# Version 0.4 - 2011-11-07 - Complete re-write. - Jeff White
+#####
+
+script="${0##*/}"
 chkconfigbin="/sbin/chkconfig"
+temp_dir="/tmp/.$script"
+num_bad_enabled_daemons_found="0"
+num_bad_enabled_daemons_disabled="0"
+num_good_disabled_daemons_found="0"
+num_good_disabled_daemons_enabled="0"
 
-#Determine RHEL version
+function _print_stderr { # Usage: _print_stderr "Some error text"
+echo "$1" 1>&2
+}
+
+function _print_stderr_then_exit { # Usage: _print_stderr_then_exit "Some error text" exit_number
+echo "$1" 1>&2
+rm -rf "$temp_dir"
+exit $2
+}
+
+function _print_stdout_then_exit { # Usage: _print_stdout_then_exit "Some error text" exit_number
+echo "$1"
+rm -f "$temp_dir"
+exit $2
+}
+
+mkdir -p "$temp_dir"
+
+#Determine if we are on and what the RHEL version is.
 if [ -f /etc/redhat-release ];then
-  isrhel5=$(awk '{if (/5/&&!/2\./&&!/3\./&&!/4\./) { print "1";nextfile } else { print "0" }}' /etc/redhat-release)
-  isrhel6=$(awk '{if (/6\./&&!/2\./&&!/3\./&&!/4\./&&!/5\./) { print "1";nextfile } else { print "0" }}' /etc/redhat-release)
+  if awk '{if (/5/&&!/2\./&&!/3\./&&!/4\./) { exit 0;nextfile } else { exit 1 }}' /etc/redhat-release;then
+    isrhel5="1"
+  elif awk '{if (/6\./&&!/2\./&&!/3\./&&!/4\./&&!/5\./) { exit 0;nextfile } else { exit 1 }}' /etc/redhat-release;then
+    isrhel6="1"
+  else
+    _print_stderr_then_exit "ERROR - $LINENO - Unable to determine what version of RHEL this box is." 1
+  fi
 else
-  echo "# $LINENO Error - This box does not appear to be RHEL."
-  exit 0
+  _print_stderr_then_exit "ERROR - $LINENO - This box does not appear to be RHEL." 1
 fi
 
-#Prep
-: > /tmp/badcurservices.lst
-: > /tmp/goodservices.lst
-: > /tmp/curgoodservicedisabled.lst
-
-if [ "$($chkconfigbin --list | grep -c 'xinetd based services')" != "0" ];then
-  echo "Note: Found xinetd based services, you'll have to control them manually."
-fi
-
-if [ "$isrhel5" = "1" ];then
-echo "System appears to be RHEL 5"
-  cat << EOF > /tmp/goodservices.lst #Add good services here!
+if [ "$isrhel5" = "1" ];then #Add daemons you want to be enabled here!
+echo "System appears to be RHEL 5."
+  cat << EOF > "$temp_dir/daemons_to_be_enabled"
 acpid
 auditd
 crond
@@ -50,10 +83,11 @@ syslog
 vmware-tools
 xinetd
 netbackup
+vxpbx_exchanged
 EOF
 elif [ "$isrhel6" = "1" ];then
-echo "System appears to be RHEL 6"
-  cat << EOF > /tmp/goodservices.lst #Add good services here!
+echo "System appears to be RHEL 6."
+  cat << EOF > "$temp_dir/daemons_to_be_enabled"
 acpid
 auditd
 crond
@@ -76,50 +110,52 @@ vmware-tools
 sendmail
 xinetd
 netbackup
+vxpbx_exchanged
 EOF
 else
-  echo "# $LINENO Error - This box does not appear to be RHEL 5 or 6."
-  exit 0
-fi
-  
-#This section disables bad services on RHEL.
-$chkconfigbin --list | grep ':on' | cut -f1 | while read -r eachcurrentenableddaemon;do
-  grep $eachcurrentenableddaemon /tmp/goodservices.lst &> /dev/null
-    if [ "$?" != "0" ];then
-      echo "$eachcurrentenableddaemon" >> /tmp/badcurservices.lst
-    fi
-  done
-if [ $(cat /tmp/badcurservices.lst | wc -l) != "0" ];then
-  echo "Found $(cat /tmp/badcurservices.lst | wc -l) bad services."
-  for eachbadservice in $(cat /tmp/badcurservices.lst);do
-  read -p "Would you like to disable $eachbadservice? y or n: " answertodisablebadservices
-    if [ "$answertodisablebadservices" = "y" ];then
-      echo "Disabling $eachbadservice."
-      $chkconfigbin --level 0123456 $eachbadservice off
-    else
-      echo "Leaving $eachbadservice alone."
-    fi
-  done
-else 
-  echo "No bad services were found."
+  _print_stderr_then_exit "ERROR - $LINENO - This box does not appear to be RHEL 5 or 6."
 fi
 
-#This section enables good services on RHEL.
-for eachgoodservice in $(cat /tmp/goodservices.lst);do
-  $chkconfigbin --list | grep $eachgoodservice | awk '!/:on/&&!/xinetd based/ {print $1}' >> /tmp/curgoodservicedisabled.lst
-done
-sed '/ntpdate/d' /tmp/curgoodservicedisabled.lst > /tmp/curgoodservicedisabled.lst
-if [ $(cat /tmp/curgoodservicedisabled.lst | wc -l) != "0" ];then
-  echo "Found $(cat /tmp/curgoodservicedisabled.lst | wc -l) good services not enabled."
-  for eachgoodcurrentservicenoton in $(cat /tmp/curgoodservicedisabled.lst);do
-    read -p "Would you like to enable $eachgoodcurrentservicenoton? y or n: " answertoenablegoodservices
-    if [ "$ANSWERTOENABLEGOODSERVICES" = "y" ];then
-      echo "Enabling $eachgoodcurrentservicenoton."
-      $chkconfigbin $eachgoodcurrentservicenoton on
-    else
-      echo "Leaving $eachgoodcurrentservicenoton alone."
-    fi
-  done
-else
-  echo "All good services are set to start on boot."
+$chkconfigbin --list | awk '/:on/ {print $1}' > "$temp_dir/currently_enabled_daemons"
+$chkconfigbin --list | awk '/0:off/&&/1:off/&&/2:off/&&/3:off/&&/4:off/&&/5:off/&&/6:off/ {print $1}' > "$temp_dir/currently_disabled_daemons"
+
+if $chkconfigbin --list | grep "xinetd based services" > /dev/null;then
+  echo "Note: Found xinetd based services, you'll have to control them manually."
 fi
+  
+#This section disables daemons we don't want.
+for each_currently_enabled_daemon in $(cat "$temp_dir/currently_enabled_daemons");do
+  grep "$each_currently_enabled_daemon" "$temp_dir/daemons_to_be_enabled" > /dev/null
+  if [ "$?" != "0" ];then
+    num_bad_enabled_daemons_found=$(($num_bad_enabled_daemons_found + 1))
+    read -p "Would you like to disable $each_currently_enabled_daemon? y or n: " user_response
+    if [ "$user_response" = "y" ];then
+      $num_bad_enabled_daemons_disabled=$(($num_bad_enabled_daemons_disabled + 1))
+      echo "Disabling $each_currently_enabled_daemon."
+      $chkconfigbin --level 0123456 $each_currently_enabled_daemon off
+    else
+      echo "Leaving $each_currently_enabled_daemon alone."
+    fi
+  fi
+done
+
+echo "Found $num_bad_enabled_daemons_found unwanted daemons which were enabled, disabled $num_bad_enabled_daemons_disabled of them."
+
+#This section enables the daemons we do want.
+for each_currently_disabled_daemon in $(cat "$temp_dir/currently_disabled_daemons");do
+  grep "$each_currently_disabled_daemon" "$temp_dir/daemons_to_be_enabled" > /dev/null
+  if [ "$?" = "0" ];then
+    num_good_disabled_daemons_found=$(($num_good_disabled_daemons_found + 1))
+    read -p "Would you like to enable $each_currently_disabled_daemon? y or n: " user_response
+    if [ "$user_response" = "y" ];then
+      num_good_disabled_daemons_enabled=$(($num_good_disabled_daemons_enabled + 1))
+      echo "Enabling $each_currently_disabled_daemon."
+      $chkconfigbin $each_currently_disabled_daemon on
+    else
+      echo "Leaving $each_currently_disabled_daemon alone."
+    fi
+  fi
+done
+
+echo "Found $num_good_disabled_daemons_found wanted daemons which were disabled, enabled $num_good_disabled_daemons_enabled of them."
+#rm -rf "$temp_dir"

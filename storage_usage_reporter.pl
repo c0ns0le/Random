eeacc9ac46427847a31ef78de239a35c2f0c5353
Jeff White <jwhite530@gmail.com>
2012-03-16 14:55:29 -0400

    Switched largest user/group section to use TB instead of GB, clarified file counts as files/directories. - Jeff White

--- storage_usage_reporter.pl
+++ storage_usage_reporter.pl
@@ -16,6 +16,7 @@ use strict; #Enforce 'good' programming rules
 
 ##### Revision history
 #
+# 0.2 - 2012-03-17 - Switched largest user/group section to use TB instead of GB, clarified file counts as files/directories.. - Jeff White
 # 0.1 - 2012-03-16 - Initial version. - Jeff White
 #
 #####
@@ -56,7 +57,7 @@ while (<>) {
 for my $user (keys %user_numfiles) {
   my $average_file_size = sprintf "%.2f", $user_datausage{$user} / $user_numfiles{$user};
   print "User: $user\n";
-  print "Number of files: $user_numfiles{$user}\n";
+  print "Number of files/directories: $user_numfiles{$user}\n";
   print "Data usage: " . sprintf("%.2f", $user_datausage{$user} / 1024 / 1024) . " MB\n";
   print "Average file size: " . sprintf("%.2f", $average_file_size / 1024 / 1024) . " MB\n";
 }
@@ -65,7 +66,7 @@ for my $user (keys %user_numfiles) {
 for my $group (keys %group_numfiles) {
   my $average_file_size = sprintf "%.2f", $group_datausage{$group} / $group_numfiles{$group};
   print "Group: $group\n";
-  print "Number of files: $group_numfiles{$group}\n";
+  print "Number of files/directories: $group_numfiles{$group}\n";
   print "Data usage: " . sprintf("%.2f", $group_datausage{$group} / 1024 / 1024) . " MB\n";
   print "Average file size: " . sprintf("%.2f", $average_file_size / 1024 / 1024) . " MB\n";
 }
@@ -75,22 +76,22 @@ use vars qw/$total_num_files $total_datausage/;
 print "\n##### Summary\n";
 print "Number of users: " . keys(%user_numfiles) . "\n";
 print "Number of groups: " . keys(%group_numfiles) . "\n";
-print "Total number of files: $total_num_files\n";
+print "Total number of files/directories: $total_num_files\n";
 print "Total file size: " . sprintf("%.2f", $total_datausage / 1024 / 1024 / 1024 / 1024) . " TB\n";
 print "Average file size: " . sprintf("%.2f", $total_datausage / $total_num_files / 1024 / 1024) . " MB\n";
-print "Average number of files per user: " . sprintf("%.2f", $total_num_files / keys(%user_numfiles)) . "\n";
-print "Average number of files per group: " . sprintf("%.2f", $total_num_files / keys(%group_numfiles)) . "\n";
+print "Average number of files/directories per user: " . sprintf("%.2f", $total_num_files / keys(%user_numfiles)) . "\n";
+print "Average number of files/directories per group: " . sprintf("%.2f", $total_num_files / keys(%group_numfiles)) . "\n";
 print "\nLargest users:\n";
 my $user_loop_count;
 foreach my $user (sort {$user_datausage{$b} <=> $user_datausage{$a}} keys %user_datausage) {
-  print "$user " . sprintf("%.2f", $user_datausage{$user} / 1024 / 1024 / 1024) . " GB \n";
+  print "$user " . sprintf("%.2f", $user_datausage{$user} / 1024 / 1024 / 1024 / 1024) . " TB \n";
   $user_loop_count++;
   last if ($user_loop_count >= 5);
 }
 print "\nLargest groups:\n";
 my $group_loop_count;
 foreach my $group (sort {$group_datausage{$b} <=> $group_datausage{$a}} keys %group_datausage) {
-  print "$group " . sprintf("%.2f", $group_datausage{$group} / 1024 / 1024 / 1024) . " GB \n";
+  print "$group " . sprintf("%.2f", $group_datausage{$group} / 1024 / 1024 / 1024 / 1024) . " TB \n";
   $group_loop_count++;
   last if ($group_loop_count >= 5);
 }

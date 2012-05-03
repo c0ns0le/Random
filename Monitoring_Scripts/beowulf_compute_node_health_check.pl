23e1ab05625d7844cfd9a57305fa6d36241ca89f
Jeff White <jwhite530@gmail.com>
2012-05-03 13:36:50 -0400

    Added -n option to check only the nodes we were told to check rather than checking all of them, fixed a regex in the Infiniband section.

--- Monitoring_Scripts/beowulf_compute_node_health_check.pl
+++ Monitoring_Scripts/beowulf_compute_node_health_check.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 # Description: Perl script to check the health of compute nodes in a Beowulf HPC cluster
 # Written By: Jeff White of the University of Pittsburgh (jaw171@pitt.edu)
-# Version: 4 (2012-5-3)
-# Last change: Added NetCool ticket tag to syslog message for local scratch space usage
+# Version: 5 (2012-5-3)
+# Last change: Added -n option to check only the nodes we were told to check rather than checking all of them,
+# fixed a regex in the Infiniband section.
 
 ##### License
 # This script is released under version three (3) of the GNU General Public License (GPL) of the 
@@ -25,15 +26,21 @@ my $bpstat = "/usr/bin/bpstat";
 
 # Defaults for some options
 my @mounts;
+my $nodes = "0-241";
 
 GetOptions('h|help' => \my $helpopt,
 	   'm|mounts=s' => \@mounts,
+	   'n|nodes:s' => \$nodes,
           ) || die "Incorrect usage, use -h for help.\n";
 
 if ($helpopt) {
-  print "Usage: $0 -m /scratch -m /home\n";
-  print "-m | --mounts : Mount points to check\n";
   print "-h | --help : Show this help\n";
+  print "-m | --mounts : Mount points to check.  Default: None\n";
+  print "-n | --nodes : Which nodes to check.  Default: 0-241\n";
+  print "\nExamples:\n";
+  print "Check nodes 0 through 9 including their /home mount: $0 -m /home -n 0..9\n";
+  print "Check nodes 0 through 9 and 15 including multiple mount points: $0 -m /home -m /scratch_global -n 0-9,15\n";
+  print "\nWarning: The -n option doesn't check for correct syntax so use the corect syntax or expect problems.\n";
   exit;
 }
 
@@ -56,7 +63,7 @@ sub generate_random_string {
 setlogsock("unix");
 openlog($0, "nonul,pid", "user") or die "Unable to open syslog connection\n";
 
-for my $bpstat_line (`$bpstat --long`) {
+for my $bpstat_line (`$bpstat --long $nodes`) {
   chomp $bpstat_line;
 
 
@@ -89,6 +96,7 @@ for my $bpstat_line (`$bpstat --long`) {
     default_ssh_opts => [-o => "ConnectionAttempts=2", -o => "StrictHostKeyChecking no"]
   );
   if ($ssh->error) {
+#     syslog("LOG_ERR", "NOC-NETCOOL-TICKET: Failed to establish SSH connection to node $node_number.\n. -- $0.")
     warn BOLD RED "ERROR: Failed to establish SSH connection to node $node_number: " . $ssh->error;
     next;
   }
@@ -123,17 +131,8 @@ for my $bpstat_line (`$bpstat --long`) {
 
 
   # Check the node's Infiniband
-  # Has IB:
-  # 1,2,3,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,
-  # 53,54,55,56,57,58,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,
-  # 93,94,95,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,
-  # 120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,
-  # 144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,
-  # 168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,
-  # 192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210,211,212,213,214,215,
-  # 216,217,218,219,220,221,222,223,225,226,227,228,229,230,231,232,233,234,235,236,237,238,239,240,241,
   if (($node_number =~ m/^[4-9]$/) or # Which nodes to skip
-     ($node_number =~ m/^4[0-9]/) or
+     ($node_number =~ m/^4[0-9]$/) or
      ($node_number =~ m/^5[0-2,9]$/) or
      ($node_number =~ m/^6[0-6]$/)) {
     print "Infiniband check disabled, skipping\n";
@@ -164,7 +163,7 @@ for my $bpstat_line (`$bpstat --long`) {
 
     # Get the scratch space info
     my ($stdout, $stderr) = $ssh->capture2("df -hP /scratch");
-    $ssh->error and die "Failed to get /scratch info on node $node_number: " . $ssh->error;
+    $ssh->error and warn "Failed to get /scratch info on node $node_number: " . $ssh->error;
 
     # Get the free space
     my $local_scratch_used_space = (split(/\s+/,$stdout))[11];
@@ -173,9 +172,18 @@ for my $bpstat_line (`$bpstat --long`) {
     # Check the free space
     if ($local_scratch_used_space < 95) {
       print "Filesystem /scratch usage is OK (${local_scratch_used_space}% used).\n";
+
+#       # Write out the usage so we can track it (needed so we can alert only if usage has been high for multiple checks by this 
+#       # script rather than alerting every time we this script runs)
+#       $ssh->system("echo \"$local_scratch_used_space:0\" > /var/tmp/.scratch_usage")
+#       $ssh->error and warn BOLD RED "Failed to update scratch free space file on node $node_number: " . $ssh->error;
+
     } else {
       warn BOLD RED "PROBLEM: Filesystem /scratch is ${local_scratch_used_space}% full on node $node_number.\n";
       syslog("LOG_ERR", "NOC-NETCOOL-TICKET: Filesystem /scratch is ${local_scratch_used_space}% full on node $node_number. -- $0.");
+
+      # Write out the usage so we can track it
+
     }
 
   }

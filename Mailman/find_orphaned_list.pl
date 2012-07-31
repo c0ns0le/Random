0934c9d749891073a06b2fc154f784fda8d35df8
Jeff White <jwhite530@gmail.com>
2012-07-31 15:57:02 -0400

    Many fixes and enhancements

--- Mailman/find_orphaned_list.pl
+++ Mailman/find_orphaned_list.pl
@@ -3,8 +3,8 @@ use strict;
 use warnings;
 # Description: Find orphaned Mailman lists and Postini aliases
 # Written by: Jeff White of the University of Pittsburgh (jaw171@pitt.edu)
-# Version: 1.1
-# Last change: Fixed the regex for the orphaned alias
+# Version: 2
+# Last change: Many fixes and enhancements.
 
 # License
 # This script is released under version three of the GNU General Public License (GPL) of the 
@@ -15,6 +15,7 @@ use warnings;
 
 # To get the Mailman admins:
 # /usr/local/mailman/bin/list_admins --all | sed 's/,//' | awk '{print "addalias jwtest@pitt.edu, "$2"@list.pitt.edu"}'
+#
 # To get the Postini aliases:
 # sed 's/"//g' cs.pitt.edu.dump | nawk '/@list.pitt.edu$/ && !/-subscribe/ && !/-unsub/ && !/-admin/ && !/-bounces/ && !/-confirm/ && !/-join/ && !/-leave/ && !/-owner/ && !/-request/ {print "addalias jwtest@pitt.edu, "$1"@cs.pitt.edu"}'
 # sed 's/"//g' math.pitt.edu.dump | nawk '/@list.pitt.edu$/ && !/-subscribe/ && !/-unsub/ && !/-admin/ && !/-bounces/ && !/-confirm/ && !/-join/ && !/-leave/ && !/-owner/ && !/-request/ {print "addalias jwtest@pitt.edu, "$1"@math.pitt.edu"}'
@@ -90,33 +91,36 @@ unless (open($PASSWD_UPDATE_FILE, "+<", $passwd_update_file)) {
 my @passwd_update_file = <$PASSWD_UPDATE_FILE>;
 
 
-# Create an array of each terminated account
+# Create a hash of each terminated account
 print "Getting list of terminated users.\n" if ($verbose);
-my @terminated_users;
+my %terminated_users;
 for my $each_line (@passwd_update_file) {
   chomp $each_line;
 
-  if ($each_line =~ m/^modify:t/) {
-    my $user = (split(m/:/, $each_line))[2];
-    print "Found terminated user: $user\n" if ($verbose);
+  # Skip lines that are not user modifications
+  next if ($each_line !~ m/^modify:/);
 
-    # See if the user was already unterminated, skip them if so
-    if (grep(m/^modify:a:\Q$user\E/, @passwd_update_file)) {
-      print "Skipping user '$user', terminated and unterminated in the same run.\n" if ($verbose);
-      next;
-    }
-    
-    push (@terminated_users, $user);
+  my ($mod_type,$user) = (split(m/:/, $each_line))[1,2];
+
+  if ($mod_type eq "t") {
+    print "Found terminated user: $user\n" if ($verbose);
+    $terminated_users{$user} = "t";
+    next;
+  }
+
+  # If the mod type is a (active) and we already have a t (terminated) for this user,
+  # they have been unterminated.  Remove them from the hash.
+  if (($terminated_users{$user}) and ($mod_type eq "a") and ($terminated_users{$user} eq "t")) {
+    print "Unterminated user: $user\n" if ($verbose);
+    delete($terminated_users{$user});
   }
 
 }
 
-if (!$terminated_users[0]) {
+unless (scalar(keys(%terminated_users))) {
   log_info("No terminated users found.");
   exit 0;
 }
-
-
 
 #
 # Check if a Mailman list is orphaned (owner is gone)
@@ -131,47 +135,57 @@ unless (open($LIST_OWNERS_FILE, "<", $list_owners_file)) {
 
 
 # Loop through each list see if an owner is gone
-# WARNING: Mailman owners are emails, not users.  We can't tell which account each email maps to.
+# WARNING: Mailman owners are emails, not users.  We can't tell which account each email address maps to.
 # This means the code below may give false results since we can't check non-pitt email addresses and
 # we assume the email of a terminated account would be user@ instead of alias@ (such as jeff.white@).
 print "Getting list of lists and their owners.\n" if ($verbose);
 my $num_list_orphans = 0;
 for my $each_line (<$LIST_OWNERS_FILE>) {
   chomp $each_line;
+  my $num_term_owners = 0;
 
   # Get the list name from the line
   my $list_name = (split(m/\s+/, $each_line))[1];
   $list_name =~ s/,//;
 
   # Get the list owners from the line
-  my $list_owner = $each_line;
-  $list_owner =~ s/^.*Owners: //;
-  $list_owner =~ s/\s+//;
+  my $list_owners = $each_line;
+  $list_owners =~ s/^.*Owners: //;
+  $list_owners =~ s/\s+//;
 
 #   print "List: $list_name\n";
 #   print "Owners: $list_owner\n";
+  my @owners = split(m/,/, $list_owners);
+#   print "Num owners: ", scalar(@owners), "\n";
+#   print "Owners: @owners\n";
 
-  for my $each_terminated_user (@terminated_users) {
-    # If 'user@' for each terminated user exists as an admin of a list
-    # and there's no other admin (no , character) then assume the terminated
-    # user is the only admin of the list.
-    if (($list_owner =~ m/\Q$each_terminated_user\E@/) and ($list_owner !~ m/,/)) {
-      print "Found orhpaned list: $list_name => $list_owner\n";
+  # For each owner...
+  for my $each_owner (@owners) {
+    # Skip non-pitt email address
+    next unless ($each_owner =~ m/\@[A-Za-z0-9._%+-]*pitt\.edu/i);
+
+    # Get the raw username out of the address (see the warning above)
+    my $user = $each_owner;
+    $user =~ s/@.*$//;
+
+    # Check if the user was terminated
+    $num_term_owners++ if ($terminated_users{$user});
+
+    if ($num_term_owners == scalar(@owners)) {
+      print "Found orphaned list: $list_name => @owners\n";
       $num_list_orphans++;
     }
+
   }
   
 }
 close $LIST_OWNERS_FILE;
 
-
-
 #
 # Check if a Postini alias is orphaned (owner is gone)
 #
 
-# Open the Postini alias database and read it into a hash
-print "Reading Postini alias database.\n" if ($verbose);
+# Open the Postini alias database
 my $POSTINI_ALIASES_FILE;
 unless (open($POSTINI_ALIASES_FILE, "<", $postini_aliases_file)) {
   log_error("Failed to open file '$postini_aliases_file'", "NOC-NETCOOL-ALERT");
@@ -189,7 +203,7 @@ for my $each_line (<$POSTINI_ALIASES_FILE>) {
   my $user = $owner;
   $user =~ s/@.*$//;
 
-  if (grep(m/^\Q$user\E$/, @terminated_users)) {
+  if ($terminated_users{$user}) {
     print "Found orphaned alias: $alias => $owner\n";
     $num_postini_orphans++;
   }
@@ -200,13 +214,13 @@ close $POSTINI_ALIASES_FILE;
 
 # Truncate the passwd_update file
 # There's a race condition here as it could have been updated as we were working but....whatever, I don't care
-truncate($PASSWD_UPDATE_FILE, 0);
+# truncate($PASSWD_UPDATE_FILE, 0);
 close $PASSWD_UPDATE_FILE;
 
 #
 # All done, print our final output.
 #
 
-print "Terminated users: ", scalar(@terminated_users), "\n";
-print "Mailman Postini alias orphans: $num_postini_orphans\n";
+print "\nTerminated users: ", scalar(keys(%terminated_users)), "\n";
 print "Mailman list orphans: $num_list_orphans\n";
+print "Mailman Postini alias orphans: $num_postini_orphans\n";

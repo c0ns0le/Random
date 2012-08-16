3ccec6159beb4173fefa2a69197f02c9a119068d
Jeff White <jwhite530@gmail.com>
2012-08-16 13:33:32 -0400

    Output a CSV instead of colon-delimited, pretty up the code a bit

--- dns_lookup_report.pl
+++ dns_lookup_report.pl
@@ -1,56 +1,61 @@
 #!/usr/bin/perl
-#Description: Perl script to create a simple report of IPs and their DNS entries.
-#Written By: Jeff White of The Univeristy of Pittsburgh (jaw171@pitt.edu)
+use warnings;
+use strict;
+# Description: Create a simple report of IPs and their DNS entries
+# Written by: Jeff White of the University of Pittsburgh (jaw171@pitt.edu)
+# Version: 1.0
+# Last change: Output a CSV instead of colon-delimited, pretty up the code a bit
 
-##### License
-# This script is released under version three (3) of the GNU General Public License (GPL) of the 
+# License
+# This script is released under version three of the GNU General Public License (GPL) of the 
 # Free Software Foundation (FSF), the text of which is available at http://www.fsf.org/licensing/licenses/gpl-3.0.html.
 # Use or modification of this script implies your acceptance of this license and its terms.
 # This is a free script, you are free to change and redistribute it with the terms of the GNU GPL.
-# There is NO WARRANTY, not even for FITNESS FOR A PARTICULAR USE to the extent permitted by U.S. law.
-#####
+# There is NO WARRANTY, not even for FITNESS FOR A PARTICULAR USE to the extent permitted by law.
 
-##### Revision history
-#
-# 0.1 - 2012-02-10 - Initial version. - Jeff White
-#
-#####
-
-use warnings; #Print warnings
-#use strict; #Enforce 'good' programming rules
+use Getopt::Long;
 use Socket;
 
-if ( @ARGV != 1 ){
-  print "Usage: $0 file\n";
-  die "A file with a list of IPs or hostnames is required as an argument";
+GetOptions('h|help' => \my $helpopt,
+          ) || die "Invalid usage, use -h for help.\n";
+
+if ($helpopt) {
+  print "Create a simple report of IPs and their DNS entries (from STDIN or file args)\n";
+  print "License: GNU General Public License (GPL) v3.\n\n";
+  print "Usage: $0 < /path/to/list\n"; 
+  print "-h | --help : Show this help\n";
+  exit;
 }
 
 #Print a header
-print "IP : Hostname\n";
+print "IP,Hostname\n";
 
-while (defined(my $eachline = <>)) {
-
-  chomp $eachline;
-
-  if ($eachline =~ m/^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5]).([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$/) {
-    $ipaddr = $eachline;
+while (defined(my $line = <>)) {
+  chomp $line;
+  my ($hostname, $ipaddr);
+  
+  # If it looks like an IP...
+  if ($line =~ m/^([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5]).([01]?\d\d?|2[0-4]\d|25[0-5])\.([01]?\d\d?|2[0-4]\d|25[0-5])$/) {
+    $ipaddr = $line;
     my $ipaddr_binary = inet_aton("$ipaddr");
     $hostname = gethostbyaddr($ipaddr_binary, AF_INET);
   }
+  # ...otherwise it must be hostname
   else {
-    $hostname = $eachline;
+    $hostname = $line;
     my $ipaddr_binary = gethostbyname($hostname);
     $ipaddr = inet_ntoa($ipaddr_binary)
   }
 
-  if ($hostname && $ipaddr) {
-    print "$ipaddr : $hostname\n";
+  # Print the results for this entry
+  if (($hostname) and ($ipaddr)) {
+    print "$ipaddr,$hostname\n";
   }
-  elsif ($hostname && !$ipaddr) {
-    print "UNKNOWN : $hostname\n";
+  elsif (($hostname) and (!$ipaddr)) {
+    print "UNKNOWN,$hostname\n";
   }
-  elsif ($ipaddr && !$hostname) {
-    print "$ipaddr : UNKNOWN\n";
+  elsif (($ipaddr) and (!$hostname)) {
+    print "$ipaddr,UNKNOWN\n";
   }
 
 }

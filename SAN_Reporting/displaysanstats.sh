e27413d090988e149396ec9666a8ac340c97b4d3
Jeff White <jwhite530@gmail.com>
2012-08-23 11:55:49 -0400

    SAN_Reporting/displaysanstats.sh: Added ASM disk exclusion to the prdrc boxes

--- SAN_Reporting/displaysanstats.sh
+++ SAN_Reporting/displaysanstats.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+shopt -s -o nounset
 #Description: Bash script to parse the output of senddiskstats.sh.
 #Written By: Jeff White of The Univeristy of Pittsburgh (jaw171@pitt.edu)
-#Version Number: 0.1
-#Revision Date: 5-26-11
-#License: This script is released under version three (3) of the GNU General Public License (GPL) of the FSF, 
-#+the text of which is available at http://www.fsf.org/licensing/licenses/gpl-3.0.html
-##This is a free script, you are free to change and redistribute it.  There is NO WARRANTY, to the extent permitted by law.
-shopt -s -o nounset
+# Version: 1.0
+# Last change: Added ASM disk exclusion to the prdrc boxes
+
+# License
+# This script is released under version three of the GNU General Public License (GPL) of the 
+# Free Software Foundation (FSF), the text of which is available at http://www.fsf.org/licensing/licenses/gpl-3.0.html.
+# Use or modification of this script implies your acceptance of this license and its terms.
+# This is a free script, you are free to change and redistribute it with the terms of the GNU GPL.
+# There is NO WARRANTY, not even for FITNESS FOR A PARTICULAR USE to the extent permitted by law.
 
 #HOW TO:
 #1. Each server will send a "df -k" and if it is Sol10 or higher, a "zpool list" to the list unix-san-stats@list.pitt.edu.
@@ -77,6 +81,8 @@ ls -1 $parseddiskstatsdir | $sedbin 's/\.edu.*/.edu/' | $sortbin | $uniqbin | wh
     $sedbin -e '/c1t0d0s6/d' ${parseddiskstatsdir}/${eachserver}.df > ${parseddiskstatsdir}/${eachserver}.df.new
     mv ${parseddiskstatsdir}/${eachserver}.df.new ${parseddiskstatsdir}/${eachserver}.df
   elif [ "$eachserver" = "devrac-a-01.cssd.pitt.edu" -o "$eachserver" = "devrac-a-02.cssd.pitt.edu" -o "$eachserver" = "devrac-a-03.cssd.pitt.edu" -o "$eachserver" = "devrac-a-04.cssd.pitt.edu" ];then
+    $sedbin -e '/asm/d' ${parseddiskstatsdir}/${eachserver}.df > ${parseddiskstatsdir}/${eachserver}.df.new
+  elif [ "$eachserver" = "prdrac-a-01.cssd.pitt.edu" -o "$eachserver" = "prdrac-a-02.cssd.pitt.edu" -o "$eachserver" = "prdrac-a-03.cssd.pitt.edu" -o "$eachserver" = "prdrac-a-04.cssd.pitt.edu" -o "$eachserver" = "prdrac-a-05.cssd.pitt.edu" ];then
     $sedbin -e '/asm/d' ${parseddiskstatsdir}/${eachserver}.df > ${parseddiskstatsdir}/${eachserver}.df.new
     mv ${parseddiskstatsdir}/${eachserver}.df.new ${parseddiskstatsdir}/${eachserver}.df
   elif [ "$eachserver" = "dwdd01.cssd.pitt.edu" -o "$eachserver" = "dwdd02.cssd.pitt.edu" ];then

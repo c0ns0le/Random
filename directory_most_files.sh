76fa9752161c04c3b32e4c3659398649b0210d5e
Jeff White <jwhite530@gmail.com>
2012-12-20 14:08:29 -0500

    Removing directory_most_files.sh

--- directory_most_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Written by: rdcwayx of unix.com
-
-node_number=${NODE:=${1:?"Path must be an arguement"}}
-
-find $1 -type f |awk  '{$NF="";a[$0]++}END{for (i in a) print a[i],i }' FS=\/ OFS=\/ | sort -rn | head

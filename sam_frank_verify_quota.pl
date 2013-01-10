5700972ca09e4a4be914dff087bdc1bc504e5ac9
Jeff White <jwhite530@gmail.com>
2013-01-10 17:15:08 -0500

    Added --user options to set a single user's quota

--- sam_frank_verify_quota.pl
+++ sam_frank_verify_quota.pl
@@ -3,8 +3,8 @@ use strict;
 use warnings;
 # Description: Verify filesystems quotas are correct and set them if not
 # Written by: Jeff White of the University of Pittsburgh (jaw171@pitt.edu)
-# Version: 1.2
-# Last change: Renamed storage servers
+# Version: 2
+# Last change: Added --user options to set a single user's quota
 
 # License
 # This script is released under version three of the GNU General Public License (GPL) of the 
@@ -24,6 +24,7 @@ use Sys::Hostname;
 
 GetOptions('h|help' => \my $helpopt,
            'd|display' => \my $do_display,
+           'u|user=s' => \my $user_name,
           ) || die "Invalid usage, use -h for help.\n";
 
 if ($helpopt) {
@@ -32,6 +33,7 @@ if ($helpopt) {
   print "Usage: $0 [options]\n"; 
   print "-h | --help : Show this help\n";
   print "-d | --display : Display every user's quota rather than checking for mismatches\n";
+  print "-u | --user someusername : Set a single user's quota to what LDAP says without asking\n";
   exit;
 }
 
@@ -42,36 +44,36 @@ my $hostname = hostname();
 if ($hostname eq "s-home2b.frank.sam.pitt.edu") {
   $filesystem = "/data/home2";
   $quota_attr = "quotaHome2";
-  print "Partner node: s-home2a\n";
+  print "Partner node: s-home2a\n" unless ($user_name);
 }
 elsif ($hostname eq "s-home0b.frank.sam.pitt.edu") {
   $filesystem = "/data/home";
   $quota_attr = "quotaHome0";
-  print "Partner node: s-home0a\n";
+  print "Partner node: s-home0a\n" unless ($user_name);
 }
 elsif ($hostname eq "s-home1a.frank.sam.pitt.edu") {
   $filesystem = "/data/home1";
   $quota_attr = "quotaHome1";
-  print "Partner node: s-home1b\n";
+  print "Partner node: s-home1b\n" unless ($user_name);
 }
 elsif ($hostname eq "s-home1b.frank.sam.pitt.edu") {
   $filesystem = "/data/home1";
   $quota_attr = "quotaHome1";
-  print "Partner node: s-home1a\n";
+  print "Partner node: s-home1a\n" unless ($user_name);
 }
 elsif ($hostname eq "s-home0a.frank.sam.pitt.edu") {
   $filesystem = "/data/home";
   $quota_attr = "quotaHome0";
-  print "Partner node: s-home0b\n";
+  print "Partner node: s-home0b\n" unless ($user_name);
 }
 elsif ($hostname eq "s-misc0.frank.sam.pitt.edu") {
-  print "Storage5 has no quotas.\n";
+  print "Storage5 has no quotas.\n" unless ($user_name);
   exit;
 }
 elsif ($hostname eq "s-home2a.frank.sam.pitt.edu") {
   $filesystem = "/data/home2";
   $quota_attr = "quotaHome2";
-  print "Partner node: s-home2b\n";
+  print "Partner node: s-home2b\n" unless ($user_name);
 }
 elsif ($hostname eq "headnode1.frank.sam.pitt.edu") {
   $filesystem = "/data/gscratch1";
@@ -97,6 +99,52 @@ my $ldap_object = Net::LDAP->new(
 my $bind_mesg = $ldap_object->bind();
 
 $bind_mesg->code && die "Failed to bind with the LDAP server: " . $bind_mesg->error;
+
+
+#
+# If we were asked to set just one user's quota get their quota from LDAP
+#
+if ($user_name) {
+
+  # Search LDAP
+  my $ldap_quota_search_result = $ldap_object->search(
+    base => "ou=person,ou=people,dc=frank,dc=sam,dc=pitt,dc=edu",
+    scope => "sub",
+    timelimit => 30,
+    filter => "(uid=$user_name)",
+    attrs => [$quota_attr],
+  );
+
+  $ldap_quota_search_result->code && die $ldap_quota_search_result->error;
+
+  # Get the search results
+  my @ldap_quota_entries = $ldap_quota_search_result->entries;
+  my $ldap_quota = $ldap_quota_entries[0]->get_value($quota_attr);
+
+  # Disconnect from LDAP
+  $ldap_object->unbind;
+  $ldap_object->disconnect;
+  
+  # Ensure we could find the quota
+  unless ($ldap_quota) {
+    print BOLD RED "Missing quota: $user_name\n";
+    exit;
+  }
+
+  # Get the user's uidNumber and the device where the filesystem is
+  my $device = Quota::getqcarg( $filesystem );
+  my $uid_number = (getpwnam($user_name))[2];
+
+  # Set the quota
+  my $soft_qouta_kb = ($ldap_quota - 10) * 1024 * 1024;
+  my $hard_quota_kb = $ldap_quota * 1024 * 1024;
+
+  Quota::setqlim( $device, $uid_number, $soft_qouta_kb, $hard_quota_kb, 0, 0, 0, 0 );
+
+  # We're done with what we were asked to do, exit
+  exit 0;
+
+}
 
 
 #
@@ -131,11 +179,7 @@ for my $each_entry (@ldap_quota_entries) {
   }
 }
 
-
-#
 # Disconnect from LDAP
-#
-
 $ldap_object->unbind;
 $ldap_object->disconnect;
 
@@ -167,7 +211,6 @@ while (my($user, $ldap_quota) = each(%ldap_quota)) {
     $local_hard_quota = "?";
   }
   
-
   # Print the each user's quota if we were asked to
   if ($do_display) {
     print "$user: $ldap_quota GB in LDAP, $local_hard_quota GB on local system\n";
@@ -185,7 +228,7 @@ while (my($user, $ldap_quota) = each(%ldap_quota)) {
         my $soft_qouta_kb = ($ldap_quota - 10) * 1024 * 1024;
         my $hard_quota_kb = $ldap_quota * 1024 * 1024;
 
-        Quota::setqlim( $device, $uid_number, $soft_qouta_kb, $hard_quota_kb, 0, 0, 0, 0 );
+        Quota::setqlim( $device, $uid_number, $soft_qouta_kb, $hard_quota_kb, 0, 0, 0, 0 ) or die;
       }
     }
   }
